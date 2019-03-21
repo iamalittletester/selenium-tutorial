52360212c19b8f2db779b4bf318c376fa1e5eead
iamalittletester <capreoara6@gmail.com>
2019-03-21 21:46:28 +0200

    Open page tests.

--- src/test/java/tutorialSolution/openPage/OpenPageTest.java
+++ src/test/java/tutorialSolution/openPage/OpenPageTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
@@ -27,12 +30,25 @@ public class OpenPageTest {
     }
 
     @Test
-    public void getPageProperties() {
+    public void getPageProperties() throws MalformedURLException {
         //open a valid URL
         driver.get("http://www.example.com");
         //check that expected URL was opened
         assertEquals("http://www.example.com/", driver.getCurrentUrl());
         //check that the title of the page is correct
         assertEquals("Example Domain", driver.getTitle());
+
+        //other valid urls
+        driver.get("https://www.example.com");
+        driver.get("http://example.com");
+        driver.get("https://example.com");
+
+        //open a page with navigate.to() and String parameter
+        driver.navigate().to("https://www.example.com");
+        assertEquals("https://www.example.com/", driver.getCurrentUrl());
+
+        //open a page with navigate.to() and URL parameter
+        driver.navigate().to(new URL("http://example.com"));
+        assertEquals("http://example.com/", driver.getCurrentUrl());
     }
 }
